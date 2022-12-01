4e184e782ca82968ab44eb9a2ad957353ff71167
Gift-koech <koechgift93@gmail.com>
2022-12-01 11:54:11 +0300

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    belongs_to :author
+    has_many :post_tags
+end
+class PostTag < ApplicationRecord
+    belongs_to :Post
+    belongs_to :tag
+
+end
+
+class Tag < ApplicationRecord
+    has_many :post_tags
+    has_many :posts, through: :post_tags
 end
